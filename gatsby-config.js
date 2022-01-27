4c4c974b3b8c3b6a479e50fae48dca286ddaed4c
S.Aizawa <showta4696@gmail.com>
2022-01-27 22:46:37 +0900

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,24 @@
 module.exports = {
-    siteMetadata: {
-      title: `gatsby-portfolio`,
-        siteUrl: `https://www.yourdomain.tld`,
+  siteMetadata: {
+    title: `gatsby-portfolio`,
+    description: "Hello Gatsby"
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-rss-feed`,
+      options: {
+        url: `https://qiita.com/mottox2/feed`,
+        name: `Qiita`,
+      }
     },
-    plugins: [
-
-    ]
+    `gatsby-transformer-yaml`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: "works",
+        path: `${__dirname}\\src\\works.yaml`,
+      }
+    },
+    "gatsby-plugin-react-helmet"
+  ]
 }
---
+++ src/pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+
+const AboutPage = (props) => {
+    return (
+        <div>
+            <Helmet>
+                <title>My Gatsby Site</title>
+                <meta name="description" content="My first gatsby website" />
+            </Helmet>
+            <p>Hello Gatsby</p>
+        </div>
+    )
+}
+
+export default AboutPage;
